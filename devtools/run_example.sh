c9ad93f785d17e12c95a63981e6ace2e817d0571
victor <vili@nvidia.com>
2018-01-05 15:34:03 +0800

    update the commands 	modified:   cuda_gdb.sh 	modified:   cuda_nvprof.sh 	modified:   run_example.sh 	modified:   sp_profile.sh

--- devtools/run_example.sh
+++ devtools/run_example.sh
@@ -1,10 +1,12 @@
 #mpirun -n XXX `which relion_refine_mpi` --i Particles/shiny_2sets.star --ctf --iter 25 --tau2_fudge 2 --particle_diameter 360 --K 200 --zero_mask --oversampling 1 --psi_step 6 --offset_range 5 --offset_step 2 --norm --scale --random_seed 0 --o class2d
 TOP=`pwd`/..
 exec_proc=$TOP/build/bin/relion_refine_mpi
-data=$TOP/RELION_Benchmarks/Particles/shiny_2sets.star
+BMDir=$TOP/RELION_Benchmarks
 
+cd $BMDir
 rm -fr class2d
 mkdir class2d
-mpirun -n 20 $exec_proc --i $data --ctf --iter 2 --tau2_fudge 2 --particle_diameter 360 --K 200 --zero_mask --oversampling 1 --psi_step 6 --offset_range 5 --offset_step 2 --norm --scale --random_seed 0 --o class2d
+
+mpirun -n 20 $exec_proc --i Particles/shiny_2sets.star --ctf --iter 2 --tau2_fudge 2 --particle_diameter 360 --K 200 --zero_mask --oversampling 1 --psi_step 6 --offset_range 5 --offset_step 2 --norm --scale --random_seed 0 --o class2d
 
 
--- devtools/sp_profile.sh
+++ devtools/sp_profile.sh
@@ -1,12 +1,12 @@
 sp="/raid/tools/SP/SystemProfiler-linux-internal-3.9.3364-05e1bbd/Target-x86_64/x86_64/sp"
 TOP=`pwd`/..
 exec_proc=$TOP/build/bin/relion_refine_mpi
-data=$TOP/RELION_Benchmarks/Particles/shiny_2sets.star
+BMDir=$TOP/RELION_Benchmarks
 
+cd $BMDir
 rm -fr class2d
 mkdir class2d
 
-cmd="mpirun -n 20 $exec_proc --i $data --ctf --iter 2 --tau2_fudge 2 --particle_diameter 360 --K 200 --zero_mask --oversampling 1 --psi_step 6 --offset_range 5 --offset_step 2 --norm --scale --random_seed 0 --o class2d"
-
+cmd="mpirun -n 20 $exec_proc --i Particles/shiny_2sets.star --ctf --iter 2 --tau2_fudge 2 --particle_diameter 360 --K 200 --zero_mask --oversampling 1 --psi_step 6 --offset_range 5 --offset_step 2 --norm --scale --random_seed 0 --o class2d"
 rm -fr ./*.QDSTRM
 $sp profile --delay=2 --duration=10 -o mgemm.QDSTRM -t cuda,cublas,curand,pthread $cmd
