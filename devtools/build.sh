6c682df777039f21c7491d4657a2921251f846fd
victor <vili@nvidia.com>
2018-01-11 16:58:58 +0800

    update build scripts

--- devtools/build.sh
+++ devtools/build.sh
@@ -2,10 +2,12 @@ TOP=`pwd`/..
 cd $TOP
 git pull
 #git checkout 9a8dfbae0d78e7ece3847a053e10ffccd7ab18d8
+rm build
 mkdir build
 cd build
+# disable gui if build in docker
 cmake .. -DGUI=OFF -DCUDA=ON -DCUDA_ARCH=60 \
--DGUI=ON -DCMAKE_EXE_LINKER_FLAGS=-L$(dirname $(which nvcc))/../lib64 \
+-DGUI=OFF -DCMAKE_EXE_LINKER_FLAGS=-L$(dirname $(which nvcc))/../lib64 \
 -DCMAKE_SHARED_LINKER_FLAGS=-L$(dirname $(which nvcc))/../lib64
 make -j
 
--- devtools/sp_profile.sh
+++ devtools/sp_profile.sh
@@ -1,4 +1,4 @@
-sp="/raid/tools/SP/SystemProfiler-linux-internal-3.9.3364-05e1bbd/Target-x86_64/x86_64/sp"
+sp="/raid/tools/SP/SystemProfiler-linux-public-3.9.3747-52a8955/Target-x86_64/x86_64/sp"
 TOP=`pwd`/..
 exec_proc=$TOP/build/bin/relion_refine_mpi
 BMDir=$TOP/RELION_Benchmarks
@@ -8,5 +8,6 @@ rm -fr class2d
 mkdir class2d
 
 cmd="mpirun -n 20 $exec_proc --i Particles/shiny_2sets.star --ctf --iter 2 --tau2_fudge 2 --particle_diameter 360 --K 200 --zero_mask --oversampling 1 --psi_step 6 --offset_range 5 --offset_step 2 --norm --scale --random_seed 0 --o class2d"
-rm -fr ./*.QDSTRM
-$sp profile --delay=2 --duration=10 -o mgemm.QDSTRM -t cuda,cublas,curand,pthread $cmd
+echo "running $cmd"
+rm -fr ./*.qdstrm
+$sp profile --delay=30 --duration=10 -o mgemm.qdstrm -t cuda,cudnn,cublas,curand,pthread $cmd
